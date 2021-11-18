f0344ac49f6f721a386d9be20ff1ba12aa91271b
neerajk <neerajk@somnoware.com>
2021-11-18 18:08:47 +0530

    📦 NEW: add till proof-of-work algo.

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,20 +1,54 @@
+const sha256 = require('sha256');
+
 class Blockchain {
     constructor() {
         this.chain = [];
-        this.newTransactions = [];
+        this.pendingTransactions = [];
+        this.createNewBlock(100, '0', '0');
     }
     createNewBlock(nonce, previousBlockHash, hash) {
         const newBlock = {
             index: this.chain.length + 1,
             timestamp: Date.now(),
-            transactions: this.newTransactions,
+            transactions: this.pendingTransactions,
             nonce: nonce,
             hash: hash,
             previousBlockHash: previousBlockHash,
         };
-        this.newTransactions = [];
+        this.pendingTransactions = [];
         this.chain.push(newBlock);
         return newBlock;
+    }
+
+    getLastBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+
+    createNewTransaction(amount, sender, recipient) {
+        const newTransaction = {
+            amount,
+            sender,
+            recipient
+        };
+
+        this.pendingTransactions.push(newTransaction);
+        return this.getLastBlock().index + 1;
+    }
+
+    hashBlock(previousBlockHash, currentBlockData, nonce) {
+        const dataAsString = previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
+        const hash = sha256(dataAsString);
+        return hash;
+    }
+
+    proofOfWork(previousBlockHash, currentBlockData) {
+        let nonce = 0;
+        let hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+        while (hash.substring(0, 4) !== '0000') {
+            nonce++;
+            hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+        }
+        return nonce;
     }
 }
 
